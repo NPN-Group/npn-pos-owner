46f9dd6189f4974de9677a8f028700cd5c29c8b4
Prataad <pondaeroz008y@gmail.com>
2024-12-06 01:44:56 +0700

    chore: remove unused file in table

--- src/shared/components/Table.tsx
+++ src/shared/components/Table.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import {useState} from 'react'
-import {Table,TableProps} from '../types/table.type'
+import {Table,TableProps} from '../types/table'
 const Tablecomponent: React.FC<TableProps> = ({Tables, isActive, onClick}) => {
 
   return (
