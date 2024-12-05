eab5464ca66ed7b3eefa146b37b01d838fb8c5c0
Chollasak Anuwareepong <chollsak@Chollasaks-MacBook-Air.local>
2024-12-06 05:34:53 +0700

    feat menu page setup

---
+++ src/app/(private)/menu/page.tsx
@@ -0,0 +1,12 @@
+import { MainLayout } from '@/shared/components'
+import React from 'react'
+
+export default function MenuPage() {
+  return (
+    <MainLayout className="flex-1 p-4 overflow-y-auto">
+        <div>
+            Menu Page
+        </div>
+    </MainLayout>
+  )
+}
