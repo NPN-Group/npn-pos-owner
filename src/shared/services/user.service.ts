17252e2223b5042429b44eb2893722594a966ad7
Prataad <pondaeroz008y@gmail.com>
2024-12-28 19:08:09 +0700

    refactor: update fetchUser URL with API_URL constant

--- src/shared/services/user.service.ts
+++ src/shared/services/user.service.ts
@@ -1,8 +1,9 @@
 import { APIResponse, User } from "@/shared/types";
 import { fetcher } from "../lib";
+import { API_URL } from "../common/constants";
 
 export const UserService = {
     fetchUser: async (): Promise<APIResponse<User>> => {
-        return fetcher<User>("users/me");
+        return fetcher<User>(`${API_URL}/api/users/me`);
     }
 }
